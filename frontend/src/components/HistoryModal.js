d3aa980e4cb4dceae52364c5ea75ec241c947a1d
leejinjae123 <cat0182@daum.net>
2025-12-23 18:21:12 +0900

    front-err_fix

--- frontend/src/components/HistoryModal.js
+++ frontend/src/components/HistoryModal.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Card } from './Common';
 
 const HistoryModal = ({ onClose, allRecords }) => {
   const [currentDate, setCurrentDate] = useState(new Date());
